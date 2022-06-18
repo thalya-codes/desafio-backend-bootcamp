10ceb31259a349eab222bc03d6dbf70635508d94
Thalya Stéffany <thalyacoiimbra@gmail.com>
2022-06-18 15:12:26 -0300

    Instalação e configuração do cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import  sequelize from"./config/database.config.js"
+import cors from "cors"
 import categoriesRoutes from "./routes/categories.routes.js";
 import productsRoutes from "./routes/products.routes.js"
 
@@ -7,8 +8,10 @@ sequelize.sync().then(() => console.log("db is ready"));
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 app.use(categoriesRoutes);
 app.use(productsRoutes);
+
 
 const port = process.env.PORT || 3000;
 
