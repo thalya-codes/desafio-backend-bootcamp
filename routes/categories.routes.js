3f6f3db98d28beb85c249fa75a8cd47d05321a5d
Thalya Stéffany <thalyacoiimbra@gmail.com>
2022-06-18 15:20:46 -0300

    Configuração do cors

--- routes/categories.routes.js
+++ routes/categories.routes.js
@@ -7,18 +7,27 @@ import {
     deleteCategory
 } from"../controllers/Categories.controller.js"
 
+import cors from "cors";
+
+export let corsOptions = {
+    origin: "https://bootcamp-code-like-a-girl.herokuapp.com/",
+    optionsSuccessStatus: 200
+};
+   
+
+
 const categoriesRoutes = Router();
 
-categoriesRoutes.get("/",(req,res) =>  res.send("Tudo okay"));
+categoriesRoutes.get("/",cors(corsOptions),(req,res) =>  res.send("Tudo okay"));
 
-categoriesRoutes.post("/categories",create);
+categoriesRoutes.post("/categories",cors(corsOptions),create);
 
-categoriesRoutes.get("/categories", getAll);
+categoriesRoutes.get("/categories",cors(corsOptions), getAll);
 
-categoriesRoutes.get("/categories/:id", getOne);
+categoriesRoutes.get("/categories/:id",cors(corsOptions), getOne);
 
-categoriesRoutes.put("/categories/:id", update);
+categoriesRoutes.put("/categories/:id",cors(corsOptions), update);
 
-categoriesRoutes.delete("/categories/:id", deleteCategory);
+categoriesRoutes.delete("/categories/:id",cors(corsOptions),deleteCategory);
 
 export default categoriesRoutes;
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -7,18 +7,19 @@ import {
     deleteProduct
 } from"../controllers/Products.controller.js"
 import uploads from "../config/multer.config.js";
-
+import cors from "cors";
+import { corsOptions } from "./categories.routes.js";
 
 const productsRoutes = Router();
 
-productsRoutes.post("/products",uploads.single("image"),create);
+productsRoutes.post("/products",cors(corsOptions),uploads.single("image"),create);
 0
-productsRoutes.get("/products",getAll);
+productsRoutes.get("/products",cors(corsOptions),getAll);
 
-productsRoutes.get("/products/:id",getOne);
+productsRoutes.get("/products/:id",cors(corsOptions),getOne);
 
-productsRoutes.put("/products/:id",update);
+productsRoutes.put("/products/:id",cors(corsOptions),update);
 
-productsRoutes.delete("/products/:id", deleteProduct) 
+productsRoutes.delete("/products/:id",cors(corsOptions),deleteProduct);
 
 export default productsRoutes;
