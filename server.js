3d5ec9cb1e15ef25131ea5b8b47e3f59c83339cc
Thalya Stéffany <thalyacoiimbra@gmail.com>
2022-06-19 12:14:24 -0300

    .

--- server.js
+++ server.js
@@ -1,3 +1,44 @@
+import express from 'express'
+import cors from 'cors'
+import  sequelize from"./config/database.config.js"
+import Categories from './models/Categories.model'
+import { json } from 'sequelize/types'
+
+var app = express()
+sequelize.sync().then(() => console.log("db is ready"));
+
+app.use(express.json())
+app.use((req, res, next) => {
+    //console.log("Acessou o Middleware!");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
+
+app.get('/categories', async  (req, res, next)  => {
+    await Categories.create(req.body);
+    return res.json({"message":"categories is inserted"});
+})
+    
+
+app.post('/categories', async  (req, res, next)  => {
+    if(req.body.category_id) {
+        await Products.create(req.body);
+        return res.json({"message":"products is inserted"});
+        
+    } else {
+        return res.json({"message": "o campo category_id é obrigatório"});
+    }
+
+})
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
+
+/*
+
 import express from "express"
 import  sequelize from"./config/database.config.js"
 import cors from "cors"
@@ -13,19 +54,19 @@ app.use(express.json());
 app.use(categoriesRoutes);
 app.use(productsRoutes);
 
-app.use((req, res, next) => {
-    //console.log("Acessou o Middleware!");
+app.use((_, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors());
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    app.use(cors);
     next();
-});
 
-app.post("/categories-test", (re,res) => {
-    return res.json(re.body)
-});
+})
+
+app.post("/categories-test",create);
+app.get("/categories-test",getAll);
 
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log("server is running"));
+*/
