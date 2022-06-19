798e872f20013ea398bfdb6913e826f6272b51ff
Thalya Stéffany <thalyacoiimbra@gmail.com>
2022-06-19 11:01:03 -0300

    .

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(categoriesRoutes);
 app.use(productsRoutes);
 
 app.use((_, res, next) => {
-    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
     app.use(cors);
     next();
