11c224993dec7c2eab8324280f1dfed046a410cd
Thalya Stéffany <thalyacoiimbra@gmail.com>
2022-06-20 17:11:45 -0300

    .

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Headers",
     "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"); 
   
-    app.use(cors());
+    app.use(cors);
     next();
 });
 
